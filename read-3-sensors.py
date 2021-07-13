e81056ebe7cbe44e15351445398129690064b246
After232 <68368921+After232@users.noreply.github.com>
2021-07-13 19:15:59 +0800

    Update read-3-sensors.py

--- read-3-sensors.py
+++ read-3-sensors.py
@@ -3,10 +3,15 @@ import time
 import datetime
 from scd30_i2cx import SCD30, SCD30B, SCD30C
 
-bus1 = smbus2.SMBus(1)
-bus3 = smbus2.SMBus(3) #Aditional 12c bus, configured in config.txt
-bus4 = smbus2.SMBus(4) #Aditional 12c bus, configured in config.txt
-address = 61
+# Create object for each sensor
+scd30A = SCD30()
+scd30B = SCD30B()
+scd30C = SCD30C()
+
+# Set measurement interval. Minimum is 2 seconds.
+measureTimeA = 2
+measureTimeB = 15
+measureTimeC = 15
 
 recordtime = datetime.datetime.now()
 
@@ -14,29 +19,29 @@ recordtime = datetime.datetime.now()
 fileNameA = ("CO2-sensor-A-readings-" + str(recordtime) + ".csv")
 fileA = open(fileNameA, "a")
 print("Created CSV file for Sensor A")
-# fileNameB = ("CO2-sensor-B-readings-" + str(recordtime) + ".csv")
-# fileB = open(fileNameB, "a")
-# print("Created CSV file for Sensor B")
-# fileNameC = ("CO2-sensor-C-readings-" + str(recordtime) + ".csv")
-# fileC = open(fileNameC, "a")
-# print("Created CSV file for Sensor C")
-
-scd30A = SCD30()
-scd30B = SCD30B()
-scd30C = SCD30C()
-
-measureTimeA = 2
-measureTimeB = 15
-measureTimeC = 15
+fileNameB = ("CO2-sensor-B-readings-" + str(recordtime) + ".csv")
+fileB = open(fileNameB, "a")
+print("Created CSV file for Sensor B")
+fileNameC = ("CO2-sensor-C-readings-" + str(recordtime) + ".csv")
+fileC = open(fileNameC, "a")
+print("Created CSV file for Sensor C")
 
 scd30A.set_measurement_interval(measureTimeA)
 scd30A.start_periodic_measurement()
+scd30B.set_measurement_interval(measureTimeB)
+scd30B.start_periodic_measurement()
+scd30C.set_measurement_interval(measureTimeC)
+scd30C.start_periodic_measurement()
 
 print_labels = True
 lineA = 0 # start at 0 because our header is not real data
+lineB = 0
+lineC = 0
 time.sleep(2)
 
+# Start collection of data
 while True:
+    # Sensor A 
     if lineA == 0:
         print("Printing column headers")
         timestampA = "Time,"
@@ -53,9 +58,51 @@ while True:
             time.sleep(measureTimeA)
         else:
             continue
-    loggedData = timestampA + getDataA
-    print(loggedData)
+    loggedDataA = timestampA + getDataA
+    print(loggedDataA)
     fileA = open(fileNameA, "a")
-    fileA.write(loggedData + "\n")
+    fileA.write(loggedDataA + "\n")
 
+    # Sensor B 
+    if lineB == 0:
+        print("Printing column headers")
+        timestampB = "Time,"
+        getDataB = "CO2 (ppm), Temp (C), Humidity (%),"
+        lineB += 1
+    else:
+        timestampB = (str(datetime.datetime.now())[0:-7] + ",")
+        if scd30B.get_data_ready():        
+            n = scd30B.read_measurement()
+            if n is not None:
+                getDataB = (f"{m[0]:.2f}, {m[1]:.2f}, {m[2]:.2f}")
+                print("Line " + str(lineB) + " of Sensor B: writing...")
+                lineB += 1
+            time.sleep(measureTimeB)
+        else:
+            continue
+    loggedDataB = timestampB + getDataB
+    print(loggedDataB)
+    fileB = open(fileNameB, "a")
+    fileB.write(loggedDataB + "\n")
 
+    # Sensor C 
+    if lineC == 0:
+        print("Printing column headers")
+        timestampC = "Time,"
+        getDataC = "CO2 (ppm), Temp (C), Humidity (%),"
+        lineC += 1
+    else:
+        timestampC = (str(datetime.datetime.now())[0:-7] + ",")
+        if scd30C.get_data_ready():        
+            n = scd30C.read_measurement()
+            if n is not None:
+                getDataC = (f"{m[0]:.2f}, {m[1]:.2f}, {m[2]:.2f}")
+                print("Line " + str(lineC) + " of Sensor C: writing...")
+                lineC += 1
+            time.sleep(measureTimeC)
+        else:
+            continue
+    loggedDataC = timestampC + getDataC
+    print(loggedDataC)
+    fileC = open(fileNameC, "a")
+    fileC.write(loggedDataC + "\n")
