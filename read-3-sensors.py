41e62b180ae8d7a42b64a7d92423dac79e3c3c14
After232 <68368921+After232@users.noreply.github.com>
2021-07-13 17:16:16 +0800

    Update read-3-sensors.py

--- read-3-sensors.py
+++ read-3-sensors.py
@@ -1,7 +1,7 @@
 import smbus2
 import time
 import datetime
-from scd30_i2c import SCD30
+from scd30_i2cx import SCD30, SCD30B, SCD30C
 
 bus1 = smbus2.SMBus(1)
 bus3 = smbus2.SMBus(3) #Aditional 12c bus, configured in config.txt
@@ -25,7 +25,11 @@ scd30A = SCD30()
 scd30B = SCD30B()
 scd30C = SCD30C()
 
-scd30A.set_measurement_interval(2)
+measureTimeA = 2
+measureTimeB = 15
+measureTimeC = 15
+
+scd30A.set_measurement_interval(measureTimeA)
 scd30A.start_periodic_measurement()
 
 print_labels = True
@@ -46,11 +50,12 @@ while True:
                 getDataA = (f"{m[0]:.2f}, {m[1]:.2f}, {m[2]:.2f}")
                 print("Line " + str(lineA) + " of Sensor A: writing...")
                 lineA += 1
-            time.sleep(2)
+            time.sleep(measureTimeA)
         else:
             continue
     loggedData = timestampA + getDataA
     print(loggedData)
     fileA = open(fileNameA, "a")
     fileA.write(loggedData + "\n")
+
 
