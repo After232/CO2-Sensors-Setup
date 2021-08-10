709f2678b902676708846a816ec3fbe8e29663e1
After232 <68368921+After232@users.noreply.github.com>
2021-08-10 13:12:06 +0800

    Update sensor-forced-calibration.py

--- sensor-forced-calibration.py
+++ sensor-forced-calibration.py
@@ -8,6 +8,16 @@ scd30A = SCD30(1)
 measureTimeA = 2
 
 scd30A.set_measurement_interval(measureTimeA)
-scd30A.forced_recalibration_reference()
+scd30A.forced_recalibration_reference(400)
 scd30A.start_periodic_measurement()
-scd30A.set_auto_self_calibration(active=True)
+
+# Run calibration. Does not record measurements taken here.
+while True:
+    if scd30A.get_data_ready():
+        measurementA = scd30A.read_measurement()
+        if measurementA is not None:
+            co2a, tempa, rha = measurementA
+            print(f"Sensor A: CO2: {co2a:.2f}ppm, temp: {tempa:.2f}'C, rh: {rha:.2f}%")
+        time.sleep(measureTimeA)
+    else:
+        continue
